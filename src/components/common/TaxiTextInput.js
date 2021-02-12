6cbfca5f256e7fdf83cecc47db0a0555bdde9c94
Test <test@test.com>
2021-02-11 22:49:01 -0800

    creating the TextInput component for the app

---
+++ src/components/common/TaxiTextInput.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {Text,View,TextInput,StyleSheet} from 'react-native';
+import fontKeys from '../../keyText/fontKeys';
+
+const TaxiTextInput = (props) =>{
+    const [value, onChangeText] = React.useState('');
+    const getInput = input => onChangeText(input);
+
+    return (
+        <TextInput
+            secureTextEntry={props.secureTextEntry?props.secureTextEntry:false}
+            placeholder= {props.placeholder? props.placeholder:'enter the text here'}
+            placeholderTextColor= 'blue'
+            placeholderStyle={{margin:100}}
+            style={props.style? props.style: styles.input}
+            keyboardType={props.keyboardType? props.keyboardType: null}
+            onChangeText={props.func? props.fun: getInput}
+            value={props.value?props.value: value}
+        />
+    )
+
+}
+export default TaxiTextInput;
+
+const styles = StyleSheet.create({
+    input:{ 
+        height: 40, 
+        width:288,
+        borderColor: 'gray',
+        borderWidth: 1 ,
+        borderRadius:3,
+        marginBottom:16,
+        fontFamily:  fontKeys.MR,
+        paddingLeft:11,
+        color:'#3F4D5F',
+        fontSize:12,
+
+    }
+});
