87ca088a691aeecfd77cd9e25d2804e8c40ca743
Test <test@test.com>
2021-02-13 14:02:17 -0800

    define background image url key

--- src/keyText/imageKeys.js
+++ src/keyText/imageKeys.js
@@ -1,4 +1,5 @@
 const imageKeys = {
-    upload: require("../assets/images/signUp/kmer.png"),   
+    upload: require("../assets/images/signUp/Oval.png"),   
+    background: require("../assets/images/background.png"),  
  };
  export default imageKeys;
