74415bcea45a6132c5faf272845c62fdc863a470
Test <test@test.com>
2021-02-24 03:05:41 -0500

    fixing the crash

--- App.js
+++ App.js
@@ -61,7 +61,7 @@ const App: () => React$Node = () => {
    */
   return (
     <NavigationContainer>
-        <ComponentBackgroundHOC Composant= { <LoginScreen/>}/>
+    <LoginRoute/>
     </NavigationContainer>
   );
 };
