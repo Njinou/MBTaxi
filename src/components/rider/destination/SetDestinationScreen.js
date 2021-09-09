8c59a55d88f8f7d0512affe8f10bbac30ded9719
Pascal Nitcheu <nitcheupascal@gmail.com>
2021-09-09 20:04:03 +0100

    removing a typo

--- src/components/rider/destination/SetDestinationScreen.js
+++ src/components/rider/destination/SetDestinationScreen.js
@@ -282,7 +282,7 @@ adaRef..child(key).remove()
               .child ('saved')
               .on('value', snapshot => {
                   if (snapshot.exists()) {
-                      // Exist! Do whatever.select
+                      // Exist! Do whatever
                 //  console.log('Saved data: ', snapshot.val());
                   setSavedPlaces(Object.values(snapshot.val()));
                   } else {
